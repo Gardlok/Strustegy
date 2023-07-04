8c2daca0977bb224b055a0af7dc248c4174086db
Anthony Gardner <59458181+Gardlok@users.noreply.github.com>
2023-07-04 02:23:41 +0000

    dev

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ use std::error::Error;
 use std::marker::PhantomData;
 
 mod inprogenitance;
+mod iterating;
 
+pub use iterating::*;
 
 
 mod test;
@@ -253,89 +255,6 @@ where
 }
 
 
-// Lending Iterator
-//                   
-pub trait LendingIterator<'a> {
-    type Item;
-
-    fn next(&'a mut self, f: &'a dyn Fn(&mut Self, &mut Self::Item)) -> Option<Self::Item>;  
-}
-
-// ContextExtendingIterator - 
-//
-pub trait ContextExtendingIterator<'a>
-where
-    Self: Sized,
-{
-    type Item;
-    type Gats: 'a + for<'b> Gat<'b, Item = Self::Item>;
-    fn gats(&'a self) -> Self::Gats;
-    fn map<'b, G>(self, f: G) -> Map<Self, G> 
-    where
-        G: FnMut(&mut Self::Item),
-        Self: Sized,
-    {
-        Map { iter: self, f }
-    }
-}
-impl<'a, I, F> ContextExtendingIterator<'a> for Map<I, F>  
-where
-    I: ContextExtendingIterator<'a>,
-    F: FnMut(&mut I::Item),
-
-{
-    type Item = I::Item;
-    type Gats = I::Gats;
-
-    fn gats(&'a self) -> Self::Gats {
-        self.iter.gats()
-    }
-    fn map<'b, G>(self, f: G) -> Map<Self, G> 
-    where
-        G: FnMut(&mut Self::Item),
-        Self: Sized,
-    {
-        Map { iter: self, f }
-    }
-}
-
-// OperationExtendingIterator 
-//
-pub trait OperationExtendingIterator<'a>
-where
-    Self: Sized,
-{
-    type Item;
-    type Gats: 'a + for<'b> Gat<'b, Item = Self::Item>;
-    fn gats(&'a self) -> Self::Gats;  
-    fn map<'b, G>(self, f: G) -> Map<Self, G> 
-    where
-        G: FnMut(&mut Self::Item),
-        Self: Sized,
-    {
-        Map { iter: self, f }
-    }
-}
-impl<'a, I, F> OperationExtendingIterator<'a> for Map<I, F>  
-where
-    I: OperationExtendingIterator<'a>,
-    F: FnMut(&mut I::Item),
-
-{
-    type Item = I::Item;
-    type Gats = I::Gats;
-
-    fn gats(&'a self) -> Self::Gats {
-        self.iter.gats()
-    }
-    fn map<'b, G>(self, f: G) -> Map<Self, G> 
-    where
-        G: FnMut(&mut Self::Item),
-        Self: Sized,
-    {
-        Map { iter: self, f }
-    }
-}
 
 // ParameterObject
 //
